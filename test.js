b588ff6c67808af18188232c1d09b23eb726802a
Marco <marco.asdman@gmail.com>
2020-04-09 21:43:16 +1000

    #DHCL-16 : update(test) : Add specific test cases (#10)

--- test.js
+++ test.js
@@ -3,23 +3,61 @@
 var test = require('tape');
 var {addClass, removeClass, toggleClass, execClass} = require('./');
 
-test('when interact with element, check for class', function (t) {
-    addClass(document.body, ['css-class']);
-    t.equal(document.body.classList.contains('css-class'), true);
+test('add class', function (t) {
+    t.equal(document.body.classList.contains('css-class-1'), false);
+    addClass(document.body, 'css-class-1');
+    t.equal(document.body.classList.contains('css-class-1'), true);
 
-    removeClass(document.body, 'css-class');
-    t.equal(document.body.classList.contains('css-class'), false);
+    t.equal(document.body.classList.contains('css-class-2'), false);
+    addClass([document.body], 'css-class-2');
+    t.equal(document.body.classList.contains('css-class-2'), true);
 
+
+    t.equal(document.body.classList.contains('css-class-3'), false);
+    addClass(document.body, ['css-class-3']);
+    t.equal(document.body.classList.contains('css-class-3'), true);
+
+
+    t.equal(document.body.classList.contains('css-class-4'), false);
+    addClass([document.body], ['css-class-4']);
+    t.equal(document.body.classList.contains('css-class-4'), true);
+
+    t.end();
+});
+
+test('remove class', function (t) {
+    t.equal(document.body.classList.contains('css-class-1'), true);
+    removeClass(document.body, 'css-class-1');
+    t.equal(document.body.classList.contains('css-class-1'), false);
+
+    t.equal(document.body.classList.contains('css-class-2'), true);
+    removeClass([document.body], 'css-class-2');
+    t.equal(document.body.classList.contains('css-class-2'), false);
+
+    t.equal(document.body.classList.contains('css-class-3'), true);
+    removeClass(document.body, ['css-class-3']);
+    t.equal(document.body.classList.contains('css-class-3'), false);
+
+    t.equal(document.body.classList.contains('css-class-4'), true);
+    removeClass([document.body], ['css-class-4']);
+    t.equal(document.body.classList.contains('css-class-4'), false);
+
+    t.end();
+});
+
+test('toggle class', function (t) {
     toggleClass([document.body], ['css-class', 'css-class-1']);
     t.equal(document.body.classList.contains('css-class'), true);
     t.equal(document.body.classList.contains('css-class-1'), true);
+    t.end();
+});
 
-    execClass(document.body, [{ add: 'css-class'}, { toggle: 'css-class-2'}, { remove: 'css-class-1'}]);
-    t.equal(document.body.classList.contains('css-class'), true);
+test('exec class', function (t) {
+    execClass(document.body, [{ add: 'css-class-3'}, { toggle: 'css-class-2'}, { remove: 'css-class-1'}]);
+    t.equal(document.body.classList.contains('css-class-3'), true);
     t.equal(document.body.classList.contains('css-class-2'), true);
     t.equal(document.body.classList.contains('css-class-1'), false);
-
-    t.end();
+   t.end();
 });
 
 test('when interact with intruder', function (t) {
