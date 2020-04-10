d3330d7d737f72592c6840e09df2338d70e915b7
Marco <marco.asdman@gmail.com>
2020-04-10 21:47:05 +1000

    #DHCL-18 : add(types) : Add declaration type (#11)

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+type NodeListLike = NodeList | Node[] | Node | EventTarget;
+type OptionList = String | String[];
+
+export declare function addClass(itmList: NodeListLike, optList: OptionList) : void;
+
+export declare function removeClass(itmList: NodeListLike, optList: OptionList) : void;
+
+export declare function toggleClass(itmList: NodeListLike, optList: OptionList) : void;
