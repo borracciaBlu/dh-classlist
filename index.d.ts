23f1541e1cfed15818b55e5cf2b90a10aa77342b
Marco <marco.asdman@gmail.com>
2020-05-11 21:21:17 +1000

    #DHCL-21 : add(types) : Add thunk types (#18)

--- index.d.ts
+++ index.d.ts
@@ -13,3 +13,16 @@ export declare function removeClass(itmList: NodeListLike, optList: OptionList) 
 export declare function toggleClass(itmList: NodeListLike, optList: OptionList) : void;
 
 export declare function execClass(itmList: NodeListLike, optList: ActionOption | ActionOption[]) : void;
+
+/**
+ *
+ * Thunks
+ *
+ */
+export declare function addClassThunk(itmList: NodeListLike, optList: OptionList) : () => void;
+
+export declare function removeClassThunk(itmList: NodeListLike, optList: OptionList) : () => void;
+
+export declare function toggleClassThunk(itmList: NodeListLike, optList: OptionList) : () => void;
+
+export declare function execClassThunk(itmList: NodeListLike, optList: ActionOption | ActionOption[]) : () => void;
